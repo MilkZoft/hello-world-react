ea1056897dfcba79f717248484401545a3b02064
Carlos Santana <csantana@beachbody.com>
2017-06-11 23:16:00 -0700

    Webpack Split Bundles & Webpack for Production

--- src/server/index.js
+++ src/server/index.js
@@ -7,19 +7,27 @@ import webpackHotMiddleware from 'webpack-hot-middleware';
 import open from 'open';
 
 // Webpack Configuration
-import webpackConfig from '../../webpack.config.dev';
+import webpackConfig from '../../webpack.config.babel';
 
 // Server Port
 const port = 3000;
 
+// Environment
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 // Express app
 const app = express();
+
+// Public
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Webpack Compiler
 const webpackCompiler = webpack(webpackConfig);
 
-app.use(webpackDevMiddleware(webpackCompiler));
-app.use(webpackHotMiddleware(webpackCompiler));
+if (isDevelopment) {
+  app.use(webpackDevMiddleware(webpackCompiler));
+  app.use(webpackHotMiddleware(webpackCompiler));
+}
 
 // Sending all the traffic to React
 app.get('*', (req, res) => {
