7839921469b2404c2217316e4289a65c77388f97
Carlos Santana <csantana@beachbody.com>
2017-06-20 15:55:52 -0700

    V10 - Adding Redux with Initial State

--- src/index.js
+++ src/index.js
@@ -2,16 +2,43 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Bluebird from 'bluebird';
+import { Provider } from 'react-redux';
 
 // Routes
 import AppRoutes from './routes';
 
+// Redux
+import configureStore from './lib/configureStore';
+
+// Reducers
+import rootReducer from './reducers';
+
 // Assets
 import './index.css';
 
+// Bluebird configuration
+window.Promise = Bluebird;
+Bluebird.config({ warnings: false });
+
+window.addEventListener('unhandledrejection', error => {
+  error.preventDefault();
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn('Unhandled promise rejection warning.', error.detail); // eslint-disable-line no-console
+  }
+});
+
+// Configuring redux store
+const store = configureStore({
+  initialState: window.initialState
+}, rootReducer);
+
 render(
-  <Router>
-    <AppRoutes />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
